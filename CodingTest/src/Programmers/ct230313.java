fd5ae4857c91eb7ed38d478bc8d5e7b2cda09377
06sha <06sha@LAPTOP-1VJACP85>
2023-03-13 00:28:50 +0900

    부족한 금액 계산하기

---
+++ CodingTest/src/Programmers/ct230313.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+class ct230313 {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+        
+        for(int i = 1; i<=count; i++){
+            sum += price*i;
+        }
+        
+        if(money < sum){
+            answer = sum - money;
+        }
+        
+        return answer;
+    }
+}
