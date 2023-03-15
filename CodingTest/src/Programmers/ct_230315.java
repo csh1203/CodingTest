25107fbac1109662a479159144ddbd0dde7448d3
06sha <shanny06@naver.com>
2023-03-15 23:04:07 +0900

    예산

---
+++ CodingTest/src/Programmers/ct_230315.java
@@ -0,0 +1,29 @@
+package Programmers;
+
+class ct_230315 {
+    public int solution(int[] d, int budget) {
+    	
+        int answer = 0;
+        int temp = 0;
+
+		for(int i = 0; i<d.length; i++) {
+			for(int j = i+1; j<d.length; j++) {
+				if(d[i]>d[j]) {
+					temp = d[i];
+					d[i] = d[j];
+					d[j] = temp;
+				}
+			}
+		}
+        
+        for(int i = 0; i<d.length; i++){
+            if(budget>=d[i]){
+                budget -= d[i];
+                answer ++;
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
