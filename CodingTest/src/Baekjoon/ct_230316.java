adde05f8d4111c0bba017c9f9c6664053153d6d9
06sha <shanny06@naver.com>
2023-03-16 22:17:57 +0900

    달팽이는 올라가고 싶다

---
+++ CodingTest/src/Baekjoon/ct_230316.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ct_230316 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt(); //낮
+		int B = scan.nextInt(); //밤
+		int V = scan.nextInt(); //목표
+		int position = 0;	
+		int days = 0;
+		
+		while(position < V) {
+			days ++;
+			position += A;
+			if(position >= V) {
+				break;
+			}
+			position -= B;
+		}
+		System.out.println(days);
+	}
+
+}
