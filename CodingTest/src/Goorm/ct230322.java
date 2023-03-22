13abf45e1c13f334896436dac43e3b6f9356fed3
06sha <shanny06@naver.com>
2023-03-22 11:57:28 +0900

    정수의 길이

---
+++ CodingTest/src/Goorm/ct230322.java
@@ -0,0 +1,13 @@
+package Goorm;
+
+import java.io.*;
+import java.util.Scanner;
+
+class ct230322 {
+	public static void main(String[] args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(scan.next().length());
+		
+	}
+}
