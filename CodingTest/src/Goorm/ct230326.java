e36342efa596a870b69aa273ad34b3b173582a55
06sha <shanny06@naver.com>
2023-03-26 01:57:36 +0900

    구름 크기 측정하기

---
+++ CodingTest/src/Goorm/ct230326.java
@@ -0,0 +1,11 @@
+package Goorm;
+
+import java.io.*;
+import java.util.Scanner;
+
+class ct230326 {
+	public static void main(String[] args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan.nextInt() * scan.nextInt());
+	}
+}
