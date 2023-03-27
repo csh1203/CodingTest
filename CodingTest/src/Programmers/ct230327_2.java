91840789a83deaf3dd3e07bcf31811081ba384ff
06sha <shanny06@naver.com>
2023-03-27 17:19:37 +0900

    콜라츠 추측

---
+++ CodingTest/src/Programmers/ct230327_2.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+class ct230327_2 {
+    public int solution(long num) {
+        int answer = 0;
+        if(num == 1) return 0;
+        
+        while(num != 1){
+            answer ++;
+            if(num%2==0)  num /= 2;
+            else num = num * 3 + 1;
+                
+            if(answer>=500) return -1;
+        }
+        return answer;
+    }
+}
