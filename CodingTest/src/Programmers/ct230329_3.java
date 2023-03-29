861968e90150900a5089b8ae565fbda20acd82b4
06sha <shanny06@naver.com>
2023-03-29 11:34:42 +0900

    두 수의 차, 두 수의 곱, 숫자 비교하기, 몫 구하기

---
+++ CodingTest/src/Programmers/ct230329_3.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+class ct230329_3 {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class ct230329_4 {
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
+
+class cr230329_5 {
+    public int solution(int num1, int num2) {
+        if(num1 == num2){
+            return 1;
+        }
+        return -1;
+    }
+}
+
+class ct230329_6 {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
