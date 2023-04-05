8a471eaef7108f73b2793c780b2447402dc8eb49
06sha <shanny06@naver.com>
2023-04-05 12:15:48 +0900

    시저 암호

---
+++ CodingTest/src/Programmers/ct230405_2.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+class ct230405_2 { //시저 암호
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == ' '){
+                answer += ' ';
+                continue;
+            } 
+            if(ch >= 'a' && ch <='z'){
+                if(ch + n > 'z'){
+                    answer += (char)(ch - 26 + n);
+                }else{
+                    answer += (char)(ch + n);
+                }
+            }else if(ch >= 'A' && ch <= 'Z'){
+                if(ch + n > 'Z'){
+                    answer += (char)(ch - 26 + n);
+                }else{
+                    answer += (char)(ch + n);
+                }
+            }
+        }
+        return answer;
+    }
+}
