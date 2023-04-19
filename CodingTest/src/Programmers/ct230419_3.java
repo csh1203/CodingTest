388d4a50570cc0794bc3e67098ba700e61c62028
06sha <shanny06@naver.com>
2023-04-19 11:06:00 +0900

    소수 찾기

---
+++ CodingTest/src/Programmers/ct230419_3.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+class ct230419_3 { //소수 찾기
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i<=n; i++){
+            boolean flag = true;
+            for(int j = 2; j<=Math.sqrt(i); j++){
+                if(i % j == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true) answer++;
+        }
+        return answer;
+    }
+}
