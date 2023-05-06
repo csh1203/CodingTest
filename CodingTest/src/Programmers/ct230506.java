2a1ddd54568eba7fa754f6e93c9d05a373685c74
06sha <shanny06@naver.com>
2023-05-06 15:43:46 +0900

    카드 뭉치

---
+++ CodingTest/src/Programmers/ct230506.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+class ct230506 { //카드 뭉치
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        int index1 = 0;
+        int index2 = 0;
+        for(int i = 0; i<goal.length; i++){
+            if(index1 < cards1.length && cards1[index1].equals(goal[i])){
+                index1++;
+            }else if(index2 < cards2.length && cards2[index2].equals(goal[i])){
+                index2++;
+            }else{
+                answer = "No";
+                break;
+            }
+        }
+        return answer;
+    }
+}
