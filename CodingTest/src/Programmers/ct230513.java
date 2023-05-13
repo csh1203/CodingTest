a3f375c00da0e6f9d7210d821bacf00e6f74a71d
06sha <shanny06@naver.com>
2023-05-13 23:27:24 +0900

    완주하지 못한 선수

---
+++ CodingTest/src/Programmers/ct230513.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+import java.util.Arrays;
+
+class ct230513 { //완주하지 못한 선수
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i = 0; i<completion.length; i++){
+            if(!(participant[i].equals(completion[i]))) return participant[i];
+        }
+        return participant[participant.length-1];
+        
+    }
+}
